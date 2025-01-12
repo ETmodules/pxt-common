5f2f3daee440e3e9c6b4b0b46abe2d4bec81e7ee
ETmodules <etmodules@outlook.com>
2025-01-12 15:07:00 +0100

    main.ts bijwerken

--- main.ts
+++ main.ts
@@ -3,6 +3,96 @@
 //% block.loc.nl="ElecTricks"
 namespace EtCommon {
 
+    /////////////////////
+    // GAMEPAD SUPPORT //
+    /////////////////////
+
+    export enum Button {
+        //% block="black-top"
+        //% block.loc.nl="zwart-boven"
+        Button1,
+        //% block="black-bottom"
+        //% block.loc.nl="zwart-onder"
+        Button2,
+        //% block="black-left"
+        //% block.loc.nl="zwart-links"
+        Button3,
+        //% block="black-right"
+        //% block.loc.nl="zwart-rechts"
+        Button4,
+        //% block="white-top"
+        //% block.loc.nl="wit-boven"
+        Button5,
+        //% block="white-bottom"
+        //% block.loc.nl="wit-onder"
+        Button6,
+        //% block="white-left"
+        //% block.loc.nl="wit-links"
+        Button7,
+        //% block="white-right"
+        //% block.loc.nl="wit-rechts"
+        Button8,
+        //% block="yellow-top"
+        //% block.loc.nl="geel-boven"
+        Button9,
+        //% block="yellow-bottom"
+        //% block.loc.nl="geel-onder"
+        Button10,
+        //% block="blue"
+        //% block.loc.nl="blauw"
+        Button11,
+        //% block="red"
+        //% block.loc.nl="rood"
+        Button12
+    }
+
+    export type gamepadHandler = () => void
+
+    let EventGamepad1: gamepadHandler
+    let EventGamepad2: gamepadHandler
+    let EventGamepad3: gamepadHandler
+    let EventGamepad4: gamepadHandler
+    let EventGamepad5: gamepadHandler
+    let EventGamepad6: gamepadHandler
+    let EventGamepad7: gamepadHandler
+    let EventGamepad8: gamepadHandler
+    let EventGamepad9: gamepadHandler
+    let EventGamepad10: gamepadHandler
+    let EventGamepad11: gamepadHandler
+    let EventGamepad12: gamepadHandler
+
+    export enum Group {
+        //% block="group 1"
+        //% block.loc.nl="groep 1"
+        Group1,
+        //% block="group 2"
+        //% block.loc.nl="groep 2"
+        Group2,
+        //% block="group 3"
+        //% block.loc.nl="groep 3"
+        Group3,
+        //% block="group 4"
+        //% block.loc.nl="groep 4"
+        Group4,
+        //% block="group 5"
+        //% block.loc.nl="groep 5"
+        Group5,
+        //% block="group 6"
+        //% block.loc.nl="groep 6"
+        Group6,
+        //% block="group 7"
+        //% block.loc.nl="groep 7"
+        Group7,
+        //% block="group 8"
+        //% block.loc.nl="groep 8"
+        Group8,
+        //% block="group 9"
+        //% block.loc.nl="groep 9"
+        Group9
+    }
+
+    let GROUP = 0
+
     ///////////////
     // MESSAGING //
     ///////////////
@@ -150,6 +240,23 @@ namespace EtCommon {
         }
     })
 
+    radio.onReceivedNumber(function(receivedNumber: number) {
+        switch (receivedNumber) {
+            case Button.Button1: if (EventGamepad1) EventGamepad1; break;
+            case Button.Button2: if (EventGamepad2) EventGamepad2; break;
+            case Button.Button3: if (EventGamepad3) EventGamepad3; break;
+            case Button.Button4: if (EventGamepad4) EventGamepad4; break;
+            case Button.Button5: if (EventGamepad5) EventGamepad5; break;
+            case Button.Button6: if (EventGamepad6) EventGamepad6; break;
+            case Button.Button7: if (EventGamepad7) EventGamepad7; break;
+            case Button.Button8: if (EventGamepad8) EventGamepad8; break;
+            case Button.Button9: if (EventGamepad9) EventGamepad9; break;
+            case Button.Button10: if (EventGamepad10) EventGamepad10; break;
+            case Button.Button11: if (EventGamepad11) EventGamepad11; break;
+            case Button.Button12: if (EventGamepad12) EventGamepad12; break;
+        }
+    })
+
     let WAIT = 0
     function sendData(data: string)
     {
@@ -200,6 +307,13 @@ namespace EtCommon {
     // GENERAL ROUTINES //
     //////////////////////
 
+    //% color="#AA6600"
+    //% block="comment: %dummy"
+    //% block.loc.nl="uitleg: %dummy"
+    //% min.defl="schrijf hier je uitleg"
+    export function comment(dummy: string) {
+    }
+
     //% block="a number from %min utai %max"
     //% block.loc.nl="een getal van %min t/m %max"
     //% min.defl=0 max.defl=10
@@ -221,12 +335,6 @@ namespace EtCommon {
         return "ALL"
     }
 
-    //% block="stop %id"
-    //% block.loc.nl="stop %id"
-    export function stop(id: string) {
-        EtCommon.setValue(id, "stop", "true")
-    }
-
     //% block="wait %time sec"
     //% block.loc.nl="wacht %time sec"
     //% min.defl=1
@@ -234,10 +342,36 @@ namespace EtCommon {
         basic.pause(time * 1000)
     }
 
-    //% color="#AA6600"
-    //% block="comment: %dummy"
-    //% block.loc.nl="uitleg: %dummy"
-    //% min.defl="schrijf hier je uitleg"
-    export function comment(dummy: string) {
+    //% block="join %group"
+    //% block.loc.nl="sluit aan bij %group"
+    export function setGroup(group: Group) {
+        GROUP = group + 1
+        radio.setGroup(GROUP)
+    }
+
+    //% block="when %button is pressed on gamepad"
+    //% block.loc.nl="wanneer op de gamepad %button wordt ingedrukt"
+
+    export function onGamepad(button: Button, programmableCode: () => void): void {
+        switch (button) {
+            case Button.Button1: EventGamepad1 = programmableCode; break;
+            case Button.Button2: EventGamepad2 = programmableCode; break;
+            case Button.Button3: EventGamepad3 = programmableCode; break;
+            case Button.Button4: EventGamepad4 = programmableCode; break;
+            case Button.Button5: EventGamepad5 = programmableCode; break;
+            case Button.Button6: EventGamepad6 = programmableCode; break;
+            case Button.Button7: EventGamepad7 = programmableCode; break;
+            case Button.Button8: EventGamepad8 = programmableCode; break;
+            case Button.Button9: EventGamepad9 = programmableCode; break;
+            case Button.Button10: EventGamepad10 = programmableCode; break;
+            case Button.Button11: EventGamepad11 = programmableCode; break;
+            case Button.Button12: EventGamepad12 = programmableCode; break;
+        }
+    }
+
+    //% block="stop %id"
+    //% block.loc.nl="stop %id"
+    export function stop(id: string) {
+        EtCommon.setValue(id, "stop", "true")
     }
 }
