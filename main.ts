2b65a8b8684f5849a12c761421dccb2099db3f3c
ETmodules <etmodules@outlook.com>
2025-01-12 19:43:05 +0100

    main.ts bijwerken

--- main.ts
+++ main.ts
@@ -229,7 +229,7 @@ namespace EtCommon {
     serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
         BUFFER = serial.readUntil(serial.delimiters(Delimiters.NewLine))
         BUFFER = "Et" + BUFFER.substr( 2) // corrects a fuzzy transmission error
-basic.showString("-" + BUFFER + "-")
+basic.showString("-"+BUFFER.substr(12,1))
         if (!BUFFER.isEmpty()) {
             // an event message is not stored
             // instead it is returned to be handled by 'onEvent'
@@ -279,6 +279,7 @@ basic.showString("-" + BUFFER + "-")
     // this applies to sensor modules
     export function askValue(module: string, signal: string) {
         let msg = module + ";A;" + signal
+basic.showString("+A")
         sendData(msg)
     }
 
@@ -287,10 +288,10 @@ basic.showString("-" + BUFFER + "-")
     // this applies to sensor modules
     export function getValue(module: string, command: string, signal: string): string {
         let val = ""
-basic.showString("+" + module + "+" + command + "+" + signal + "+")
+basic.showString("+G")
         do {
             val = g_messages.value(module, command, signal)
-            basic.pause(1)  // anable 'onDataReceived' to receive messages meanwhile
+            basic.pause(10)  // anable 'onDataReceived' to receive messages meanwhile
         }                   // instead of 'yield', which isn't part of typescript
         while (val.isEmpty())
         return val
