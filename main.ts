1096610c30395b2e34306f470aa3ea2903e5ce22
ETmodules <etmodules@outlook.com>
2025-01-26 19:37:45 +0100

    main.ts bijwerken

--- main.ts
+++ main.ts
@@ -57,7 +57,6 @@ namespace EtCommon {
                 let item = this.items[i]
                 if ((item.mod == module) &&
                     (item.sig == signal)) {
-basic.showNumber(2)
                     item.hnd(module, value)
                     return
                 }
@@ -101,8 +100,8 @@ basic.showNumber(2)
 
     serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
         let line = serial.readUntil(serial.delimiters(Delimiters.NewLine))
-        line = "Et" + line.substr( 2) // corrects a fuzzy transmission error
         if (!line.isEmpty()) {
+            line = "Et" + line.substr(2) // corrects a fuzzy transmission error
 basic.showString(line)
             let msg = new Message( line)
             events.onEvent( msg.mod, msg.sig, msg.val)
